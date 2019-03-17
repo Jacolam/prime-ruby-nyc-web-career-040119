7d5e3a6b586e7d62ef213ced048384e0e2833127
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-17 15:17:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,18 @@
 # Add  code here!
 def prime?(integer)
-
+  
   range_number = (1..integer)
-  prime_number = []  
-
-  #loop over array then pull out if prime to prime array
-  #if size of prime array == 2, then true
-
+  factors = []
+  
   range_number.each do |check_number|
-    if integer % check_number == 0
-      prime_number.push(check_number)
+    if integer % check_number == 0 
+      factors << check_number
     end
   end
-
-  if prime_number.size == 2
-    true
-  else
-    false
-  end
-
-end
+  
+  if factors.size == 2 
+    true 
+  else 
+    false 
+  end 
+end 
